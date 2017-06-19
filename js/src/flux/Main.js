32bd8fc135d21476d9c02584cec5053a0bb54d9b
martin-fctg <martin.kennedy@us.flightcentre.com>
2017-06-19 16:02:14 -0400

    Update Main.js

--- js/src/flux/Main.js
+++ js/src/flux/Main.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.listen(3000);
+
+
 import React, { Component } from 'react';
 
 import App from '../components/App';
@@ -29,40 +39,76 @@ export default class Main extends Component {
 	}
 }
 
-// DON'T use "const indexedDB = ..." if you're not in a function.
-// Moreover, you may need references to some window.IDB* objects:
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
-// (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
 
 // Let us open our database
-const request = window.indexedDB.open(Store);
-const result = Object.keys(Store).map(function(e) {
-  return [Store[e]];
-});
-console.log(result);
-console.log(Store);
+var DBOpenRequest = window.indexedDB.open("toDoList", 4);
 
-const r = 10; // require distance
-lat_range = IDBKeyRange.bound(query_lat - r, query_lat + r);
-lng_range = IDBKeyRange.bound(query_lng - r, query_lng + r);
+DBOpenRequest.onsuccess = function(event) {
+  console.log(' database initialised.');
+    
+  // store the result of opening the database in the db variable.
+  // This is used a lot below
+  db = DBOpenRequest.result;
+    
+  // Run the addData() function to add the data to the database
+  addData();
+};
 
-obj_store = db.objectStore('geopoint');
-lat_key_cursor = obj_store.index('lat').openKeyCursor(lat_range);
-lng_key_cursor = obj_store.index('lng').openKeyCursor(lng_range);
+function addData() {
+  // Create a new object ready to insert into the IDB
+  var newItem = [ { taskTitle: "Walk dog", hours: 19, minutes: 30, day: 24, month: "December", year: 2013, notified: "no" } ];
 
-const compound_index = obj_store.index('lat, lng');
-const range = IDBKeyRange.bound([query_lat - r], [query_lat + r]);
-const cursor = compound_index.openKeyCursor(range);
-cursor.onsuccess = function(e) {
-  const key = e.target.result.key();
-  const lat = key[0];
-  const lng = key[1];
-  if (lng > query_lng - r && lng < query_lng + r) {
-     // we get result
-     const req = obj_store.get(e.target.result.primaryKey());
-     req.onsuccess = function(e2) {
-        console.log(e2.target.result);
-     }
-  }
-}  
+  // open a read/write db transaction, ready for adding the data
+  var transaction = db.transaction(["toDoList"], "readwrite");
+
+  // report on the success of opening the transaction
+  transaction.oncomplete = function(event) {
+    console.log('Transaction completed: database modification finished.');
+  };
+
+  transaction.onerror = function(event) {
+    console.log('Transaction not opened due to error. Duplicate items not allowed.');
+  };
+
+  // create an object store on the transaction
+  var objectStore = transaction.objectStore("toDoList");
+  console.log(objectStore.name);
+
+  // add our newItem object to the object store
+  var objectStoreRequest = objectStore.add(newItem[0]);
+
+  objectStoreRequest.onsuccess = function(event) {
+    // report the success of our new item going into the database
+    console.log('New item added to DB.');
+  };
+};
+// const request = window.indexedDB.open(Store);
+// const result = Object.keys(Store).map(function(e) {
+//   return [Store];
+// });
+// console.log(result);
+// console.log(request);
+
+// const r = 10; // require distance
+// lat_range = IDBKeyRange.bound(query_lat - r, query_lat + r);
+// lng_range = IDBKeyRange.bound(query_lng - r, query_lng + r);
+
+// obj_store = db.objectStore('geopoint');
+// lat_key_cursor = obj_store.index('lat').openKeyCursor(lat_range);
+// lng_key_cursor = obj_store.index('lng').openKeyCursor(lng_range);
+
+// const compound_index = obj_store.index('lat, lng');
+// const range = IDBKeyRange.bound([query_lat - r], [query_lat + r]);
+// const cursor = compound_index.openKeyCursor(range);
+// cursor.onsuccess = function(e) {
+//   const key = e.target.result.key();
+//   const lat = key[0];
+//   const lng = key[1];
+//   if (lng > query_lng - r && lng < query_lng + r) {
+//      // we get result
+//      const req = obj_store.get(e.target.result.primaryKey());
+//      req.onsuccess = function(e2) {
+//         console.log(e2.target.result);
+//      }
+//   }
+// }  
