977fda557f4484d5762f39d423cb2f131cc6bbb5
kennedym103 <kennedym103@gmail.com>
2017-08-06 14:33:58 -0400

    added card border top

--- js/src/index.js
+++ js/src/index.js
@@ -41,9 +41,9 @@ db.open()
         for (var i = 0; i < location.length; ++i) {
             console.log(location[i])
             db.locations.bulkPut([{
-                spotId: location[i].spotId, 
-                lat: location[i].lat, 
-                lng: location[i].lng, 
+                spotId: location[i].spotId,
+                lat: location[i].lat,
+                lng: location[i].lng,
                 town: location[i].town
             }]);
         }
@@ -60,7 +60,7 @@ const main = (db) => {
             .and(x => (x.lng > currentLng - 2) && (x.lng < currentLng + 2))
             .toArray()
             .then(handleData)
-    })  
+    })
 }
 
 // db.table1.where("key1").between(8,12).and(function (x) { return x.key2 >= 3 && x.key2 < 18; }).toArray();
@@ -80,14 +80,14 @@ const handleData = (values) => {
         data: {
             format: "json"
         },
-    }));  
+    }));
 
     Promise.all(promises).then(resultsArr => {
        const locations = handleDataResults(resultsArr, values)
        // sort locations
 
        locations.sort( (a, b) => a.overallRating <= b.overallRating ? 1 : -1);
-       
+
        drawHtml(locations)
     })
 }
@@ -98,11 +98,11 @@ const handleDataResults = (results, values) => {
     const locations = []
 
     for (let j = 0; j < results.length; j++) {
-        
+
         for (let k = 0; k < results[j].length; k++) {
-                
+
             if (currNow < results[j][k].timestamp*1000 ) {
-             
+
                 const data = results[j][k-1];
                 data.overallRating = data.solidRating  + data.fadedRating * 2;
                 const dataWithValue = Object.assign({}, data, values[j])
@@ -121,6 +121,8 @@ const drawHtml = (arr) => {
     for (let l = 0; l < arr.length; l++){
         $('#js-cards').append(
         `<div class="card mt-4 mb-4">
+            <div class="card-border--top"
+            </div>
             <img class="card-img-top img-fluid" src="https://maps.googleapis.com/maps/api/staticmap?center=${arr[l].lat},${arr[l].lng}&zoom=13&size=500x300&key=AIzaSyBwKCefMD-LRIuQvwoGCbsFkcGKas0hjo4" alt="Card image cap">
             <div class="card-block">
                 <div class="row">
@@ -136,19 +138,19 @@ const drawHtml = (arr) => {
                     <div class="col">
                        <div class="card-text">DIRECTION</div>
                         <div>${arr[l].swell.components.primary.compassDirection}</div>
-                    
+
                        <div class="card-text">PERIOD</div>
                         <div>${arr[l].swell.components.primary.period} SECONDS</div>
                     </div>
                 </div>
                 <div class="row">
-                <div class="col"> 
+                <div class="col">
                         <div class="card-text">WIND</div>
                     </div>
                 </div>
                 <div class="row">
-                    
-                   <div class="col"> 
+
+                   <div class="col">
                         <div>${arr[l].wind.compassDirection}</div>
                     </div>
                     <div class="col">
@@ -157,12 +159,10 @@ const drawHtml = (arr) => {
                 </div>
               </div>
               <div class="row ">
-                
                   <a class="text-center col p-3" href="https://maps.google.com?q=${arr[l].lat},${arr[l].lng}">NAVIGATE</a>
-                
               </div>
               </div>
             </div>
         </div>`);
-    }    
+    }
 }
